6692a4d10c5b2ec5e489b3befa7d174e12674d0b
mathews-hos <mathewshosoares@gmail.com>
2023-06-14 14:55:38 -0300

    SDADA
    DSADADS

---
+++ vai.js
@@ -0,0 +1,17 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Por favor, digite sua entrada: ');
+var max
+process.stdin.on('data', (input) => {
+    console.log(`Você digitou: ${input}`);
+    max = parseInt(input)
+    // AQUI
+    for (let i = 0; i < max; i++) {
+        console.log(i)
+
+    }
+
+
+
+    //AQUI
+});
