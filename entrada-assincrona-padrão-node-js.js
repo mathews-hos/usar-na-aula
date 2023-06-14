e5d1870dd38cc3f9a3c4a683a1cbe073b8451e87
mathews-hos <mathewshosoares@gmail.com>
2023-06-14 16:34:03 -0300

    ae

--- entrada-assincrona-padrão-node-js.js
+++ entrada-assincrona-padrão-node-js.js
@@ -9,20 +9,11 @@ function prompt() {
 }
 sair = false
 async function main() {
-    a = await prompt()
-    console.log("digita ae 1")
-    console.log(1, await prompt())
-    console.log("digita ae 2")
-    console.log(2, await prompt())
+
+
     // use await getInput() para entrada
-    if (a == 4) {
-        sair = true
-    }
+    console.log(await prompt()) //exemplo
 
-
-    if (sair == true) {
-        process.exit()
-    }
     main()
 }
 main()
